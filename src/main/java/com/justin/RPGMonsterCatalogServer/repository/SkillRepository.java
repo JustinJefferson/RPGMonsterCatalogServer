3d88b0e2064aca1e78b88bbdb0efc5fe024453cc
JustinJefferson <justleland@gmail.com>
2019-12-18 12:24:21 -0500

    Added Skill Repository

--- src/main/java/com/justin/RPGMonsterCatalogServer/repository/SkillRepository.java
+++ src/main/java/com/justin/RPGMonsterCatalogServer/repository/SkillRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class SkillRepository {
+public interface SkillRepository extends JpaRepository<Skill, Long> {
 }
