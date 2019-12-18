8e55f66c2f156219dd9c865983c2b7266693cae1
JustinJefferson <justleland@gmail.com>
2019-12-18 13:15:32 -0500

    Created scaffolding for MonsterController. Created Element, Family, and MonsterService classes to support the Monster class.

---
+++ src/main/java/com/justin/RPGMonsterCatalogServer/model/Element.java
@@ -0,0 +1,9 @@
+package com.justin.RPGMonsterCatalogServer.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Element {
+
+    private String name;
+}
---
+++ src/main/java/com/justin/RPGMonsterCatalogServer/service/MonsterService.java
@@ -0,0 +1,7 @@
+package com.justin.RPGMonsterCatalogServer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MonsterService {
+}
