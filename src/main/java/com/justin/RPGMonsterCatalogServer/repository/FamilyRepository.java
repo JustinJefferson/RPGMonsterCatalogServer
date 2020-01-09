2fa7d318e22d41ed436ef87dafcdfc3be740894d
JustinJefferson <justleland@gmail.com>
2020-01-09 17:12:23 -0500

    Created Repositories for Element and Family models

---
+++ src/main/java/com/justin/RPGMonsterCatalogServer/repository/FamilyRepository.java
@@ -0,0 +1,7 @@
+package com.justin.RPGMonsterCatalogServer.repository;
+
+import com.justin.RPGMonsterCatalogServer.model.Family;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FamilyRepository extends JpaRepository<Family, Long> {
+}
