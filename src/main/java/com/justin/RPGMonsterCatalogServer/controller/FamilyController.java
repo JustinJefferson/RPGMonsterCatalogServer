9d46cb6a7570ea081e104bc30862afcd15c60842
JustinJefferson <justleland@gmail.com>
2020-01-14 23:23:24 -0500

    Created RESTful methods for get and post in the Family controller and service

--- src/main/java/com/justin/RPGMonsterCatalogServer/controller/FamilyController.java
+++ src/main/java/com/justin/RPGMonsterCatalogServer/controller/FamilyController.java
@@ -1,10 +1,48 @@
 package com.justin.RPGMonsterCatalogServer.controller;
 
+import com.justin.RPGMonsterCatalogServer.model.Family;
 import com.justin.RPGMonsterCatalogServer.service.FamilyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/family")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FamilyController {
 
     @Autowired
     private FamilyService service;
+
+    @PostMapping
+    public ResponseEntity<Family> post(@RequestBody Family family) {
+        Family result = service.create(family);
+        return result != null ? new ResponseEntity<>(result, HttpStatus.CREATED) : ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Family>> getAll() {
+        List<Family> ls = service.readAll();
+        if(ls != null) {
+            return new ResponseEntity<>(ls, HttpStatus.OK);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Family> get(@PathVariable Long id) {
+        Family family = service.readById(id);
+        if(family != null) {
+            return new ResponseEntity<>(family, HttpStatus.OK);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
 }
--- src/main/java/com/justin/RPGMonsterCatalogServer/service/FamilyService.java
+++ src/main/java/com/justin/RPGMonsterCatalogServer/service/FamilyService.java
@@ -1,9 +1,13 @@
 package com.justin.RPGMonsterCatalogServer.service;
 
 import com.justin.RPGMonsterCatalogServer.controller.FamilyController;
+import com.justin.RPGMonsterCatalogServer.model.Family;
 import com.justin.RPGMonsterCatalogServer.repository.FamilyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FamilyService {
@@ -12,5 +16,17 @@ public class FamilyService {
     @Autowired
     public FamilyService(FamilyRepository repository) {
         this.repository = repository;
+    }
+
+    public List<Family> readAll() {
+        return repository.findAll();
+    }
+
+    public Family readById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public Family create(Family family) {
+        return repository.save(family);
     }
 }
